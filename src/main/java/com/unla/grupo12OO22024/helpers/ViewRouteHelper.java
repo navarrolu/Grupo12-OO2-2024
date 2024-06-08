6f89ec34f10dcee5c8aa323b6472a150456081c5
maxi2594 <maxi2594@hotmail.com>
2024-06-08 17:41:15 -0300

    alta baja modificacion Producto

--- src/main/java/com/unla/grupo12OO22024/helpers/ViewRouteHelper.java
+++ src/main/java/com/unla/grupo12OO22024/helpers/ViewRouteHelper.java
@@ -10,7 +10,15 @@ public class ViewRouteHelper {
 	public final static String DEGREE_FORM = "degree/form";
 	public final static String DEGREE_NEW = "degree/new";
 
+    // PRODUCTO
+    public final static String PRODUCTO_FORM = "producto/form";
+    public final static String PRODUCTO_NEW = "producto/new";
+
+    public final static String PRODUCTO_INDEX = "producto/index";
+
     // REDIRECTS
     public final static String ROUTE_INDEX = "/index";
     public final static String DEGREE_ROOT = "/degrees/";
+
+
 }
