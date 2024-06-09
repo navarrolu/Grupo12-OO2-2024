392d095f1d7d0de180a068c22b9b0e55c35ca32e
lu <navarromartalucia@gmail.com>
2024-06-09 20:09:41 -0300

    bug arreglado. Se modifico el pom corrigiendo incompatibilidades entre dependencias. Compila

--- src/main/java/com/unla/grupo12OO22024/entities/User.java
+++ src/main/java/com/unla/grupo12OO22024/entities/User.java
@@ -5,17 +5,20 @@ import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+
+
 
 @Entity
 @Table(name = "user")
